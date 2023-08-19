ce9596861803155b73cb0fac2a95798dbadb6325
Masonord <trotf233@gmail.com>
2023-08-19 23:21:50 +0200

    refactoring

--- src/track/entity/track.entity.ts
+++ src/track/entity/track.entity.ts
@@ -1,36 +1,35 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 
-
 interface TrackEntity {
-    id: string;
-    name: string;
-    artistId: string | null;
-    albumId: string | null;
-    duration: number;
-    favoriteId: number
+  id: string;
+  name: string;
+  artistId: string | null;
+  albumId: string | null;
+  duration: number;
+  favoriteId: number;
 }
 
 export class Track implements TrackEntity {
-    @ApiProperty({format: 'uuid'})
-    public id: string;
+  @ApiProperty({ format: 'uuid' })
+  public id: string;
 
-    @ApiProperty({example: 'Track1'})
-    public name: string;
+  @ApiProperty({ example: 'Track1' })
+  public name: string;
 
-    @ApiPropertyOptional({format: 'uuid', nullable: true})
-    public artistId: string | null;
+  @ApiPropertyOptional({ format: 'uuid', nullable: true })
+  public artistId: string | null;
 
-    @ApiPropertyOptional({format: 'uuid', nullable: true})
-    public albumId: string | null;
-    
-    @ApiProperty({example: '203'})
-    public duration: number;
+  @ApiPropertyOptional({ format: 'uuid', nullable: true })
+  public albumId: string | null;
 
-    @Exclude()
-    public favoriteId: number
-    
-    constructor(props: Track) {
-        Object.assign(this, props);
-    }
+  @ApiProperty({ example: '203' })
+  public duration: number;
+
+  @Exclude()
+  public favoriteId: number;
+
+  constructor(props: Track) {
+    Object.assign(this, props);
+  }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,17 +1,17 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
-import {v4} from 'uuid'
+import { v4 } from 'uuid';
 import { Track } from './entity/track.entity';
 import { PrismaService } from 'src/nest/prismanpx/prisma.service';
 
 @Injectable()
 export class TrackService {
   constructor(private prisma: PrismaService) {}
-  
+
   public async create(createTrackDto: CreateTrackDto): Promise<any> {
     const newTrack = await this.prisma.track.upsert({
-      where: {id: v4()},
+      where: { id: v4() },
       update: {},
       create: {
         id: v4(),
@@ -19,8 +19,8 @@ export class TrackService {
         duration: createTrackDto.duration,
         albumId: createTrackDto.albumId,
         artistId: createTrackDto.artistId,
-      }
-    })
+      },
+    });
 
     return new Track(newTrack);
   }
@@ -31,22 +31,31 @@ export class TrackService {
 
   public async findOne(id: string): Promise<any> {
     const track = await this.prisma.track.findUnique({
-      where: { id }
+      where: { id },
     });
 
     if (track === null)
-      throw new HttpException("Record has not been found", HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        'Record has not been found',
+        HttpStatus.NOT_FOUND,
+      );
 
     return new Track(track);
   }
 
-  public async update(id: string, updateTrackDto: UpdateTrackDto): Promise<any> {
+  public async update(
+    id: string,
+    updateTrackDto: UpdateTrackDto,
+  ): Promise<any> {
     let track = await this.prisma.track.findUnique({
-      where: {id}
+      where: { id },
     });
 
-    if(track === null) 
-      throw new HttpException("Record has not been found", HttpStatus.NOT_FOUND);
+    if (track === null)
+      throw new HttpException(
+        'Record has not been found',
+        HttpStatus.NOT_FOUND,
+      );
 
     track = await this.prisma.track.update({
       where: { id },
@@ -54,39 +63,45 @@ export class TrackService {
         name: updateTrackDto.name,
         duration: updateTrackDto.duration,
         artistId: updateTrackDto.artistId,
-        albumId: updateTrackDto.albumId
-      } 
+        albumId: updateTrackDto.albumId,
+      },
     });
 
     return new Track(track);
   }
 
-  public async remove(id: string): Promise<any>  {
+  public async remove(id: string): Promise<any> {
     const track = await this.prisma.track.findUnique({
-      where: { id }
+      where: { id },
     });
-    const fav = await this.prisma.favorite.findUnique({where: {id: 1}, include: {tracks: true}})
+    const fav = await this.prisma.favorite.findUnique({
+      where: { id: 1 },
+      include: { tracks: true },
+    });
     const trackIndex = fav.tracks.findIndex((t) => t.id === id);
 
     if (track === null)
-      throw new HttpException("Record has not been found", HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        'Record has not been found',
+        HttpStatus.NOT_FOUND,
+      );
 
-    if(trackIndex !== -1) {
+    if (trackIndex !== -1) {
       fav.tracks.splice(trackIndex, 1);
-      
+
       await this.prisma.favorite.update({
-        where: {id: 1},
-        include: {tracks: true},
+        where: { id: 1 },
+        include: { tracks: true },
         data: {
           tracks: {
-            set: fav.tracks
-          }
-        }
+            set: fav.tracks,
+          },
+        },
       });
     }
-  
+
     await this.prisma.track.delete({
-      where: { id }
+      where: { id },
     });
   }
 }
