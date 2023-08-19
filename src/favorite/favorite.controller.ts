ba2f988b8fb9a35075d7f19da6f058554328e7e4
Masonord <trotf233@gmail.com>
2023-08-19 23:20:57 +0200

    refactoring

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -1,8 +1,27 @@
-import { Controller, Get, Post, HttpCode, Param, Delete, UseInterceptors, ClassSerializerInterceptor, ParseUUIDPipe, HttpException, HttpStatus} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  Param,
+  Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  ParseUUIDPipe,
+  HttpStatus,
+} from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
 import { Track } from 'src/track/entity/track.entity';
-import { ApiTags, ApiOperation, ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiParam, ApiNotFoundResponse, ApiUnprocessableEntityResponse, ApiNoContentResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiUnprocessableEntityResponse,
+  ApiNoContentResponse,
+} from '@nestjs/swagger';
 import { Album } from 'src/album/entities/album.entity';
 import { Artist } from 'src/artist/entities/artist.entity';
 
@@ -10,76 +29,74 @@ import { Artist } from 'src/artist/entities/artist.entity';
 @Controller('favs')
 @UseInterceptors(ClassSerializerInterceptor)
 export class FavoriteController {
-  constructor(
-    private readonly favoriteService: FavoriteService,
-  ) {}
+  constructor(private readonly favoriteService: FavoriteService) {}
 
   @Post('/track/:id')
-  @ApiOperation({summary: 'Add track to favoriter'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiUnprocessableEntityResponse({description: 'Track record doen not exist'})
-  @ApiCreatedResponse({description: 'OK', type: Track})
-
-  addTrack(@Param('id', ParseUUIDPipe) trackId: string): Promise<any>{
+  @ApiOperation({ summary: 'Add track to favoriter' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiUnprocessableEntityResponse({
+    description: 'Track record doen not exist',
+  })
+  @ApiCreatedResponse({ description: 'OK', type: Track })
+  addTrack(@Param('id', ParseUUIDPipe) trackId: string): Promise<any> {
     return this.favoriteService.addTrack(trackId);
   }
 
   @Post('/album/:id')
-  @ApiOperation({summary: 'Add album to favoriter'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiUnprocessableEntityResponse({description: 'Album record doen not exist'})
-  @ApiCreatedResponse({description: 'OK', type: Album})
-
+  @ApiOperation({ summary: 'Add album to favoriter' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiUnprocessableEntityResponse({
+    description: 'Album record doen not exist',
+  })
+  @ApiCreatedResponse({ description: 'OK', type: Album })
   addAlbum(@Param('id', ParseUUIDPipe) albumId: string): Promise<any> {
     return this.favoriteService.addAlbum(albumId);
   }
 
   @Post('/artist/:id')
-  @ApiOperation({summary: 'Add artist to favoriter'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiUnprocessableEntityResponse({description: 'Artist record doen not exist'})
-  @ApiCreatedResponse({description: 'OK', type: Artist})
-
+  @ApiOperation({ summary: 'Add artist to favoriter' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiUnprocessableEntityResponse({
+    description: 'Artist record doen not exist',
+  })
+  @ApiCreatedResponse({ description: 'OK', type: Artist })
   addArtist(@Param('id', ParseUUIDPipe) artistId: string): Promise<any> {
     return this.favoriteService.addArtist(artistId);
   }
 
   @Delete('track/:id')
-  @ApiOperation({summary: 'Remove track from favorite'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiNotFoundResponse({description: 'The track is not favorite'})
-  @ApiNoContentResponse({description: 'NO_CONTENT'})
+  @ApiOperation({ summary: 'Remove track from favorite' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiNotFoundResponse({ description: 'The track is not favorite' })
+  @ApiNoContentResponse({ description: 'NO_CONTENT' })
   @HttpCode(HttpStatus.NO_CONTENT)
-
   removeTrack(@Param('id', ParseUUIDPipe) trackId: string): Promise<void> {
     return this.favoriteService.removeTrack(trackId);
   }
 
   @Delete('artist/:id')
-  @ApiOperation({summary: 'Remove artist from favorite'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiNotFoundResponse({description: 'The artist is not favorite'})
-  @ApiNoContentResponse({description: 'NO_CONTENT'})
+  @ApiOperation({ summary: 'Remove artist from favorite' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiNotFoundResponse({ description: 'The artist is not favorite' })
+  @ApiNoContentResponse({ description: 'NO_CONTENT' })
   @HttpCode(HttpStatus.NO_CONTENT)
-
   removeArtist(@Param('id', ParseUUIDPipe) artistId: string): Promise<void> {
     return this.favoriteService.removeArtist(artistId);
   }
 
   @Delete('album/:id')
-  @ApiOperation({summary: 'Remove album from favorite'})
-  @ApiBadRequestResponse({description: 'Id is not uuid format'})
-  @ApiNotFoundResponse({description: 'The album is not favorite'})
-  @ApiNoContentResponse({description: 'NO_CONTENT'})
+  @ApiOperation({ summary: 'Remove album from favorite' })
+  @ApiBadRequestResponse({ description: 'Id is not uuid format' })
+  @ApiNotFoundResponse({ description: 'The album is not favorite' })
+  @ApiNoContentResponse({ description: 'NO_CONTENT' })
   @HttpCode(HttpStatus.NO_CONTENT)
-
   removeAlbum(@Param('id', ParseUUIDPipe) albumId: string): Promise<void> {
     return this.favoriteService.removeAlbum(albumId);
   }
 
   @Get()
-  @ApiOperation({summary: 'Get all favorites'})
-  @ApiCreatedResponse({description: 'OK', type: CreateFavoriteDto})
+  @ApiOperation({ summary: 'Get all favorites' })
+  @ApiCreatedResponse({ description: 'OK', type: CreateFavoriteDto })
   findAll(): Promise<any> {
     return this.favoriteService.findAll();
   }
--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { FavoriteController } from './favorite.controller';
-import { TrackService } from 'src/track/track.service';
-import { AlbumService } from 'src/album/album.service';
-import { ArtistService } from 'src/artist/artist.service';
-import { TrackController } from 'src/track/track.controller';
-import { AlbumController } from 'src/album/album.controller';
-import { ArtistController } from 'src/artist/artist.controller';
 import { PrismaModule } from 'src/nest/prismanpx/prisma.module';
 
 @Module({
   controllers: [FavoriteController],
   providers: [FavoriteService],
-  imports: [PrismaModule]
+  imports: [PrismaModule],
 })
 export class FavoriteModule {}
