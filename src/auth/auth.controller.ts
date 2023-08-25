136d1d1388d07bd583945256f45455ca83310459
Masonord <trotf233@gmail.com>
2023-08-26 00:46:26 +0200

    Add user's sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, HttpStatus, HttpCode, Post, Body, UseGuards, Get, Request} from '@nestjs/common';
+import { Controller, HttpStatus, HttpCode, Post, Body, UseGuards, Get, Request, UseInterceptors, ClassSerializerInterceptor} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { Public } from './decorators/public.decorator';
 
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: Record<string, any>) {
-        return this.authService.sigIn(signInDto.login, signInDto.password);
+    @Public()
+    signIn(@Body() signInDto: Record<string, any>): Promise<any> {
+        return this.authService.signIn(signInDto.login, signInDto.password);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('signup')
+    @Public()
+    signUp(@Body() createUserDto: CreateUserDto): Promise<any> {
+        return this.authService.signUp(createUserDto);
     }
 
     @UseGuards(AuthGuard)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,50 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/nest/prismanpx/prisma.service';
+import { v4 } from 'uuid';
+import { User } from 'src/user/entities/user.entity';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { hashPassword } from 'src/utils/hashing-password';
+import { comparePasswords } from 'src/utils/check-password';
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UserService,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private prisma: PrismaService
     ) {}
 
-    async sigIn(login: string, pass: string): Promise<any> {
+    async signIn(login: string, pass: string): Promise<any> {
         const user = await this.userService.findOneByLogin(login);
-        if (user?.password != pass) {
+        if ((await comparePasswords(pass, user?.password)) === false) {
             throw new UnauthorizedException();
         }
         const playload = {sub: user.id, userLogin: user.login};
         return {
             acess_token: await this.jwtService.signAsync(playload)
         };
+    }
+
+    async signUp(createUserDto: CreateUserDto): Promise<any> {
+        const hashedPassword = hashPassword(createUserDto.password);
+        
+        const user = await this.prisma.user.upsert({
+            where: {
+              id: v4(),
+            },
+            update: {},
+            create: {
+              id: v4(),
+              login: createUserDto.login,
+              password: (await hashedPassword).toString(),
+              version: 1,
+              createdAt: new Date().getTime() / 1000,
+              updatedAt: new Date().getTime() / 1000,
+            },
+          });
+      
+        return new User(user);
     }
 }
