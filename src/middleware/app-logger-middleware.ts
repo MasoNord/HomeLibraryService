77f8efc8d00a0409aae845f5bcca2fbcbcb1bcc3
Masonord <trotf233@gmail.com>
2023-08-25 23:32:22 +0200

    refactor: add colors to logs

--- src/middleware/app-logger-middleware.ts
+++ src/middleware/app-logger-middleware.ts
@@ -1,27 +1,56 @@
 import { Injectable, NestMiddleware, Logger, } from "@nestjs/common";
 import {Response, Request, NextFunction} from "express";
+import * as fs from 'fs'
 import { HttpAdapterHost } from '@nestjs/core';
+import { FileRotation } from "src/utils/file-rotation";
 
 @Injectable()
 export class AppLoggerMiddleware implements NestMiddleware  {
     constructor(private readonly httAdapterHost: HttpAdapterHost) {}
     private logger = new Logger("HTTP");
+    private filename: string = "";
+    private logfileDirectory = "./src/logs";
     
     use(request: Request, response: Response, next: NextFunction): void {
         const {httpAdapter} = this.httAdapterHost;
-
         const responseBody = {
             path: httpAdapter.getRequestUrl(request),
             method: httpAdapter.getRequestMethod(request),
+            statusCode: response.status,
+            timestamp: new Date()
         }
-        
+
+        try {
+            if (!fs.existsSync(this.logfileDirectory)) {
+                fs.mkdirSync(this.logfileDirectory);
+            }
+        }catch(err) { console.log(err); }
+
         response.on('finish', () => {
-            const { statusCode } = response;
-      
-            this.logger.log(
-                `${statusCode} {${responseBody.path}, ${responseBody.method}} route`,
-              );
-          });
+            const {statusCode} = response;
+            const contentForConsole = `${JSON.stringify(request.url)} ${JSON.stringify(request.params)} ${JSON.stringify(request.method)} ${JSON.stringify(statusCode)}`;
+            const contentForFile = `${JSON.stringify(responseBody.timestamp)} ${JSON.stringify(request.url)} ${JSON.stringify(request.params)} ${JSON.stringify(request.method)} ${statusCode} \n`;
+
+            if(statusCode >= 400 && statusCode < 500) {
+                this.logger.warn(contentForConsole);    
+                this.filename = "warn.txt";                 
+            }
+            else if(statusCode >= 200 && statusCode < 400){
+                this.logger.log(contentForConsole);
+                this.filename = "log.txt";
+            }
+            else {
+                this.logger.error(contentForConsole);
+                this.filename = "error.txt";
+            }
+            
+            fs.appendFileSync(
+                `${this.logfileDirectory}/${this.filename}`,
+                contentForFile
+            );
+
+            FileRotation(this.filename, contentForFile);
+        });
       
         next();
     }
