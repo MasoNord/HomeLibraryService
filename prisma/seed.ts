1e9274ace4c6e2d3889e2a599a517dab06e78b7c
Masonord <trotf233@gmail.com>
2023-08-27 17:32:59 +0200

    Refactoring

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,67 +15,9 @@ async function main() {
             login: "Yohoo",
             password: "qwert1234",
             createdAt: new Date().getTime() / 1000,
-            updatedAt: new Date().getTime() / 1000
+            updatedAt: new Date().getTime() / 1000,
         }
     });
-
-    // const user1 = await prisma.user.upsert({
-    //     where: {
-    //         id: v4()
-    //     },
-    //     update: {},
-    //     create: {
-    //         id: v4(),
-    //         version: 1,
-    //         login: "Yohoo",
-    //         password: "qwert123",
-    //         createdAt: new Date().getTime() / 1000,
-    //         updatedAt: new Date().getTime() / 1000
-    //     }
-    // })
-
-    // const artist = await prisma.artist.upsert({
-    //     where: {
-    //         id: v4()
-    //     },
-    //     update: {},
-    //     create: {
-    //         id: v4(),
-    //         name: "Mettalica",
-    //         grammy: false,
-    //         favoriteId: 1
-    //     }
-    // });
-
-    // const album = await prisma.album.upsert({
-    //     where: {
-    //         id: v4()
-    //     },
-    //     update: {},
-    //     create: {
-    //         id: v4(),
-    //         name: "Mettalica",
-    //         year: 2014,
-    //         artistId: artist.id,
-    //         favoriteId: 1
-    //     }
-    // });
-
-    // const track = await prisma.track.upsert({
-    //     where: {
-    //         id: v4()
-    //     },
-    //     update: {},
-    //     create: {
-    //         id: v4(),
-    //         name: "Nothing Else Matters",
-    //         duration: 388,
-    //         artistId: artist.id,
-    //         albumId: album.id,
-    //         favoriteId: 1
-    //     }
-    // });
-
     const favorite = await prisma.favorite.upsert({
         where: {
             id: 1
@@ -85,13 +27,8 @@ async function main() {
             id: 1,
         }
     });
-
     console.log({
         user,
-        // user1,
-        // artist,
-        // album,
-        // track,
         favorite
     });
 }
